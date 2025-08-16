4701a722bd7c24a844702a379c9a249b13db49f5
sergdekas <webvakula@gmail.com>
2025-08-16 17:03:51 +0300

    fix: lint and fix errors

--- src/index.js
+++ src/index.js
@@ -1,60 +1,62 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
   const numbersArr = [
-        'zero',
-        'one',
-        'two',
-        'three',
-        'four',
-        'five',
-        'six',
-        'seven',
-        'eight',
-        'nine',
-        'ten',
-        'eleven',
-        'twelve',
-        'thirteen',
-        'fourteen',
-        'fifteen',
-        'sixteen',
-        'seventeen',
-        'eighteen',
-        'nineteen'
-    ];
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
 
-    const numDecArr = [
-        'twenty',
-        'thirty',
-        'forty',
-        'fifty',
-        'sixty',
-        'seventy',
-        'eighty',
-        'ninety'
-    ];
+  const numDecArr = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
 
-    let result = "";
-    let str = number.toString();
+  let result = '';
+  const str = number.toString();
 
-    if (str.length == 1 || number < 20) {
-        result = numbersArr[number];
-    } else if (str.length == 2) {
-        result = (number % 10 == 0)
-        ? numDecArr[parseInt(str[0]-2)]
-        : numDecArr[parseInt(str[0]-2)] + " " + numbersArr[parseInt(str[1])];
-    } else if (str.length == 3) {
-        if (number % 100 == 0) {
-            result = numbersArr[parseInt(str[0])] + " hundred";
-        } else if (parseInt(str[1]+str[2]) < 20) {
-            result = numbersArr[parseInt(str[0])] + " hundred " + numbersArr[parseInt(str[1]+str[2])];
-        } else {
-            result = numbersArr[parseInt(str[0])] + " hundred " + ((str[1] != "0")
-            ? numDecArr[parseInt(str[1]-2)] + " "
-            : "") + ((str[2] != 0) ? numbersArr[parseInt(str[2])]
-            : "");
-        }
+  if (str.length === 1 || number < 20) {
+    result = numbersArr[number];
+  } else if (str.length === 2) {
+    result =
+      number % 10 === 0
+        ? numDecArr[Number(str[0] - 2)]
+        : `${numDecArr[Number(str[0] - 2)]} ${numbersArr[Number(str[1])]}`;
+  } else if (str.length === 3) {
+    if (number % 100 === 0) {
+      result = `${numbersArr[Number(str[0])]} hundred`;
+    } else if (Number(str[1] + str[2]) < 20) {
+      result = `${numbersArr[Number(str[0])]} hundred ${
+        numbersArr[Number(str[1] + str[2])]
+      }`;
+    } else {
+      result = `${numbersArr[Number(str[0])]} hundred ${
+        str[1] !== '0' ? `${numDecArr[Number(str[1] - 2)]} ` : ''
+      }${str[2] !== '0' ? numbersArr[Number(str[2])] : ''}`;
     }
+  }
 
-    result = result.trim();
-    return result;
-}
+  result = result.trim();
+  return result;
+};
